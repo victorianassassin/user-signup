0da8e5b3816df61fa5bf1784c687c2eb49735a70
Sun Baus <wobaus@gmail.com>
2017-07-01 00:03:33 -0700

    modify if statement for all input

--- main.py
+++ main.py
@@ -5,37 +5,50 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+
+
+
+
 @app.route("/signup", methods=['POST'])
 def signup():
+
+    error_username = ""
+    error_passwords = ""
+    error_verifypasswords = ""
+    error_email = ""
+
     username = request.form['username']
     passwords = request.form['passwords']
     verifypasswords = request.form['verifypasswords']
     email = request.form['email']
-
-
-    # Need to make if statement goes thru whole 4 conditino instead of return when one first condition meets.
-    
+ 
     if len(username) < 3 or len(username) > 20 or " " in username:
-        #error = " That's not a valid username"
+        error_username = " That's not a valid username"
+  
 
-        #return render_template('edit.html', error=error)
-        print ("That's not a valid username")
     if len(passwords) < 3 or len(passwords) > 20 or " " in passwords:
         error_passwords = " That's not a valid passwords"
-
-        return render_template('edit.html', error_passwords=error_passwords)
+      
 
     if not passwords == verifypasswords:
         error_verifypasswords = " Passwords don't match"
-
-        return render_template('edit.html', error_verifypasswords=error_verifypasswords)
-
-    if len(email) < 3 or len(email) > 20 or " " in email and not ("." in email or "@" in email):
-        error_email = " That's not a valid email"
         
-        return render_template('edit.html', error_email=error_email)
 
-    return render_template('confirmation.html', username=username)
+    if not email:
+        error_email = ""
+        
+        if len(email) < 3 or len(email) > 20 or " " in email or not ("." in email or "@" in email):
+            error_email = " That's not a valid email"
+    
+   
+     
+
+
+    if not error_username and error_passwords and error_verifypasswords and error_email:
+        return render_template('confirmation.html', username=username)
+    else:
+        return render_template('edit.html', error_username=error_username, error_passwords=error_passwords, error_verifypasswords=error_verifypasswords, error_email=error_email)
+        
 
 
 
