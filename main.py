081fff0b33502ff69384b6ad18deab503837b144
Sun Baus <wobaus@gmail.com>
2017-07-05 22:28:57 -0700

    preserve username and email

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ app.config['DEBUG'] = True
 
 
 
-
-
 @app.route("/signup", methods=['POST'])
 def signup():
 
@@ -21,10 +19,10 @@ def signup():
     passwords = request.form['passwords']
     verifypasswords = request.form['verifypasswords']
     email = request.form['email']
- 
+    
     if len(username) < 3 or len(username) > 20 or " " in username:
         error_username = " That's not a valid username"
-  
+        
 
     if len(passwords) < 3 or len(passwords) > 20 or " " in passwords:
         error_passwords = " That's not a valid passwords"
@@ -34,8 +32,8 @@ def signup():
         error_verifypasswords = " Passwords don't match"
         
 
-    if not email:
-        error_email = ""
+    if  not email:
+         pass #if no email input then pass
         
         if len(email) < 3 or len(email) > 20 or " " in email or not ("." in email or "@" in email):
             error_email = " That's not a valid email"
@@ -43,13 +41,15 @@ def signup():
    
      
 
-
-    if not error_username and error_passwords and error_verifypasswords and error_email:
-        return render_template('confirmation.html', username=username)
+    # check if error_var is empty string then go to confirmation page        
+    if  not error_username and not error_passwords and not error_verifypasswords and not error_email:
+         return render_template('confirmation.html', username=username)
+    
     else:
-        return render_template('edit.html', error_username=error_username, error_passwords=error_passwords, error_verifypasswords=error_verifypasswords, error_email=error_email)
+       
         
-
+        return render_template('edit.html',username=username, email=email, error_username=error_username, error_passwords=error_passwords, error_verifypasswords=error_verifypasswords, error_email=error_email)
+            
 
 
 @app.route("/")
